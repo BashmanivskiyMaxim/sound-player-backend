6669417231ca8a6915a18b60a5001cb56f609dd6
DESKTOP-GK0VM36\madma <madmaxcimka1203@gmail.com>
2023-11-15 22:36:21 +0200

    bug fix3

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module"
 
 const start = async () => {
     try {
-        const PORT = process.env.PORT || 8080;
+        const PORT = process.env.PORT || 3000;
         const app = await NestFactory.create(AppModule)
         app.enableCors()
         app.enableCors()
